39ce24152095f872581e245ab5581400c135ec7f
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-04-01 19:22:33 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,11 @@
 class BankAccount
 
+  attr_accessor :name, :balance #:status
+
+  def initialize(name, balance = nil) #status = open)
+    @name = name
+    @balance = 1000
+    #@status = status
+  end
+
 end
