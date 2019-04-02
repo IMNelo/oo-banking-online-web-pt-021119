52e6b929911da79fd19808d1ce74c9792f76394c
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-04-02 04:32:13 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,11 +1,30 @@
+require 'pry'
+
 class BankAccount
 
-  attr_accessor :name, :balance #:status
+  attr_accessor :balance, :status
+  attr_reader :name
 
-  def initialize(name, balance = nil) #status = open)
+  def initialize(name, balance = 1000, status = "open")
     @name = name
     @balance = 1000
-    #@status = status
+    @status = "open"
+  end
+
+  def deposit(add_funds)
+    @balance += deposit
+  end
+
+  def display_balance(balance)
+    @balance
+    "Your balance is $#{balance}."
+  end
+
+  def valid?
+  end
+
+  def close_account
+    self.status = "closed"
   end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,26 @@
 class Transfer
-  # your code here
+
+  attr_accessor :transfer, :sender, :receiver, :status
+
+  def initialize(transfer, sender, receiver, status = "pending")
+    @transfer = transfer
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+  end
+
+  def valid?
+    @sender
+    @receiver
+    expect(avi.valid?).to eq (true)
+    expect(amanda.valid?).to eq(true)
+    expect(transfer.valid?).to eq(true)
+  end
+
+  def execute_transaction
+  end
+
+  def reverse_transfer
+  end
+
 end
