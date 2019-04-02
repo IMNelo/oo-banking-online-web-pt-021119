848e09f4a1cf6070d6dd3020c1d82dc60bd733bf
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-04-02 07:15:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ class Transfer
 
   attr_accessor :sender, :receiver, :amount, :status
 
-  #@@all = []
+  @@all = []
 
   def initialize(sender, receiver, amount, status = "pending")
     @sender = sender
