b0f9d4f272b1dbc2ebf338a9b20655ed8cd3ea91
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-04-02 06:47:26 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -5,22 +5,33 @@ class BankAccount
   attr_accessor :balance, :status
   attr_reader :name
 
+  @@all = []
+
   def initialize(name, balance = 1000, status = "open")
     @name = name
     @balance = 1000
     @status = "open"
+    @@all << self
   end
 
-  def deposit(add_funds)
-    @balance += deposit
+  def self.all
+    @@all
   end
 
-  def display_balance(balance)
-    @balance
-    "Your balance is $#{balance}."
+  def deposit(funds_added)
+    @balance += funds_added
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
   end
 
   def valid?
+    if @balance > 0
+      @status == "open"
+    else
+      false
+    end
   end
 
   def close_account
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,19 +2,23 @@ class Transfer
 
   attr_accessor :transfer, :sender, :receiver, :status
 
+  @@all = []
+
   def initialize(transfer, sender, receiver, status = "pending")
     @transfer = transfer
     @sender = sender
     @receiver = receiver
     @status = "pending"
+    @@all << self
+  end
+
+  def self.all
+    @@all #exposes
   end
 
   def valid?
     @sender
     @receiver
-    expect(avi.valid?).to eq (true)
-    expect(amanda.valid?).to eq(true)
-    expect(transfer.valid?).to eq(true)
   end
 
   def execute_transaction
